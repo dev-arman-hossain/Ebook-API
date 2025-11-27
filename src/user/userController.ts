e8badaba45ba1c58ea828b7a896621b5c4c5c14b
Arman Hossain <web.devarman@gmail.com>
2025-11-28 04:14:38 +0600

    user model added

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel.js";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   //Validation
@@ -8,7 +9,13 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "All fields are required");
     return next(error);
   }
+  //Database call
+  const user = await userModel.findOne({email});
 
+    if (user) {
+      const error = createHttpError(400, "User already exists with this email");
+      return next(error);
+    }
   //process
   //response
   res.json({ message: "User created successfully" });
---
+++ src/user/userModel.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import type { User } from "./userTypes.ts";
+
+const userSchema = new mongoose.Schema<User>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//users
+export default mongoose.model<User>("User", userSchema);
