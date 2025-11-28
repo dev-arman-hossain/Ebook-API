c35aa0b3b125cfd339899f3e750e902947556301
Arman Hossain <web.devarman@gmail.com>
2025-11-29 03:47:19 +0600

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -59,4 +59,33 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
 
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are required"));
+  }
+
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return next(createHttpError(404, "User not found"));
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return next(createHttpError(400, "Username or password is incorrect"));
+  }
+
+  //token generation
+  const token = sign({ sub: user._id }, config.jwtSecret as string, {
+    expiresIn: "1h",
+    algorithm: "HS256",
+  });
+  res.status(200).json({ accessToken: token });
+
+  res.json({ message: "Ok" });
+};
+
+export { createUser, loginUser };
